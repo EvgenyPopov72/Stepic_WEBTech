0f383fdc5751925a0a8c94b4fc48910e2d930d50
Evgeny Popov <evgeny.popov72@gmail.com>
2016-03-20 23:28:08 +0500

    Django Auth

--- init.sh
+++ init.sh
@@ -2,6 +2,12 @@
 
 sudo apt-get update && sudo apt-get install -y mc && sudo pip install --upgrade django
 
+# Настройка базы mysql
+sudo ln -s /home/box/web/etc/mysql.cnf /etc/mysql/conf.d/stepic.cnf
+sudo /etc/init.d/mysql restart
+sudo sh manage_mysql.sh
+
+# Создание таблиц в базе и пользователя
 cd /home/box/web/ask
 python manage.py migrate
 #python manage.py shell  from django.contrib.auth.models import User \n user = User.objects.create_user('TheJohnLennon', 'lennon@thebeatles.com', 'johnpassword').save()
@@ -9,13 +15,11 @@ echo "Create user.."
 #python manage.py shell
 python manage.py createsuperuser --username=joe --email=joe@example.com
 
-sudo ln -s /home/box/web/etc/mysql.cnf /etc/mysql/conf.d/stepic.cnf
-sudo /etc/init.d/mysql restart
-sudo sh manage_mysql.sh
-
+# Настройка nginx
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
+# Настройка gunicorn
 sudo ln -s /home/box/web/etc/gunicorn.conf.old /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
